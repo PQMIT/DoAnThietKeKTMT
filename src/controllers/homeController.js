735fb3eec265d48356e37ae9190c1d4604cfe170
PQMIT <PQM2112000@gmail.com>
2023-02-26 15:33:05 +0700

    CRUD complete

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -31,15 +31,46 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
   let data = await CRUDService.getAllUser();
-  console.log("-------------------");
-  console.log(data);
-  console.log("-------------------");
   return res.render("displayCRUD.ejs", { dataTable: data });
 };
+
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id;
+  console.log(userId);
+  if (userId) {
+    let userData = await CRUDService.getUserInfoById(userId);
+
+    return res.render("editCRUD.ejs", {
+      user: userData,
+    });
+  } else {
+    return res.send("User not found");
+  }
+};
+
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  let allUsers = await CRUDService.updateUserData(data);
+  return res.render("displayCRUD.ejs", { dataTable: allUsers });
+};
+
+let deleteCRUD = async (req, res) => {
+  let id = req.query.id;
+  if (id) {
+    await CRUDService.deleteUserById(id);
+    return res.send("delete the user succeed");
+  } else {
+    return res.send("user not found");
+  }
+};
+
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
   getCRUD: getCRUD,
   postCRUD: postCRUD,
   displayGetCRUD: displayGetCRUD,
+  getEditCRUD: getEditCRUD,
+  putCRUD: putCRUD,
+  deleteCRUD: deleteCRUD,
 };
